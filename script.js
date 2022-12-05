fad1530926fab5ec8dc9288aaf91abe1e9be6924
OkePaul <57877724+OkePaul@users.noreply.github.com>
2022-12-05 15:54:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let selectedNames = ['Paul', 'Yaakov', 'John', 'Jen', 'Jason', 'Frank', 'Larry', 'Paula', 'Laura', 'Jim'];
+console.log(selectedNames);
+console.log("Hello" + " " + selectedNames[1]);
+console.log("Goodbye" + " " + selectedNames[2]);
+console.log("Goodbye" + " " + selectedNames[3]);
+console.log("Goodbye" + " " + selectedNames[4]);
+console.log("Hello" + " " + selectedNames[0]);
+console.log("Hello" + " " + selectedNames[5]);
+console.log("Hello" + " " + selectedNames[6]);
+console.log("Hello" + " " + selectedNames[7]);
+console.log("Hello" + " " + selectedNames[8]);
+console.log("Goodbye" + " " + selectedNames[9]);
